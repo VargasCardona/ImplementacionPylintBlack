4638417fe3171e5fa8ff210e953676a259718027
Mateo Loaiza <loaiza.mateo.7291@eam.edu.co>
2024-09-13 08:47:07 -0500

    Update main with methods for person and cat

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -3,27 +3,41 @@ from helpers.api_key_auth import get_api_key
 
 from starlette.responses import RedirectResponse
 
-from routes.user_route import user_router
+from .models.person import Person
+from .models.cat import Cat
 
 app = FastAPI(
-    title="Microservicio de usuarios",
+    title="Microservice of persons and little cats",
     version="2.0",
     contact={
-        "name": "Nicolas Duran Garces",
-        "url": "https://github.com/NicolasDuranGarces",
-        "email": "nicolas.duran@eam.edu.co",
+        "name": "Nicolás Vargas Cardona - Mateo Loaiza García",
+        "url": "https://github.com/VargasCardona",
     },
 )
 
 
 @app.get("/", include_in_schema=False)
 def read_root():
+    """
+    Root endpoint that redirects to the API documentation.
+
+    Returns:
+        RedirectResponse: A response object that redirects the client to the "/docs" URL.
+    """
     return
 
 
-app.include_router(
-    user_router,
-    prefix="/user",
-    tags=["user"],
-    dependencies=[Depends(get_api_key)],
-)
+@app.post("/post_person")
+def post_person(person: Person):
+    """
+    Create a new person.
+    """
+    print(person)
+
+
+@app.post("/post_cat")
+def post_cat(cat: Cat):
+    """
+    Create a new cat.
+    """
+    print(cat)
